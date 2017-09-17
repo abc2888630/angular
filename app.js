18c0015ce35c7291abebdd483c22bf0538dfe85b
51fan <abc2978723@163.com>
2017-09-17 13:33:44 +0800

    version2.0

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-define(function () {
+define(['uiRouter'],function (uiRouter) {
     var app = angular.module('myApp', []);
 
     app.controller('mainController', function ($scope) {
         $scope.hello  = 'requireJS is ok';
+        console.log($scope.hello);
     });
 
     return app;
--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
-var url = require("url");
-var mime = require('./mime.js').types;
-var port = 8800;
-http.createServer(function(request,resopnse){
-	var contentType;
-	var pathname = url.parse(request.url).pathname;
-	var ext = path.extname(pathname);
-	var realPath = ext.slice(1);
-	console.log('客户端的请求路径为：'+request.url);
-	if(pathname =="/favicon.ico"){
-		return;
-	}else if(pathname == "/"){
-		contentType = 'text/html'
-	}else{
-		contentType = mime[realPath] || "text/plain";
-	}
 
-	fs.readFile(pathname.substr(1),function(err, data){
-		if(err){
-			console.log(err);
-			resopnse.writeHead(404,{'Content-Type':contentType,'charset':'UTF8'});
-			resopnse.write('网页出现问题或者路径不存在');
+exports.start = function (){
+	var http = require('http');
+	var path = require('path');
+	var fs = require('fs');
+	var url = require("url");
+	var mime = require('./mime.js').types;
+	var port = 8800;
+	http.createServer(function(request,resopnse){
+		var contentType;
+		var pathname = url.parse(request.url).pathname;
+		var ext = path.extname(pathname);
+		var realPath = ext.slice(1);
+		console.log('客户端的请求路径为：'+request.url);
+		if(pathname =="/favicon.ico"){
+			return;
+		}else if(pathname == "/"){
+			contentType = 'text/html'
 		}else{
-			resopnse.writeHead(200,{'Content-Type':contentType,'charset':'UTF8'});
-			resopnse.write(data.toString());
+			contentType = mime[realPath] || "text/plain";
 		}
-		resopnse.end();
-	})
-}).listen(port);
-console.log('服务器成功创建，端口号为：'+port);
+
+		fs.readFile(pathname.substr(1),function(err, data){
+			if(err){
+				console.log(err);
+				resopnse.writeHead(404,{'Content-Type':contentType,'charset':'UTF8'});
+				resopnse.write('网页出现问题或者路径不存在');
+			}else{
+				resopnse.writeHead(200,{'Content-Type':contentType,'charset':'UTF8'});
+				resopnse.write(data.toString());
+			}
+			resopnse.end();
+		})
+	}).listen(port);
+	console.log('服务器成功创建，端口号为：'+port);
+}
