39d7161712392d1c3b14074b4a86f7bc9fb655e8
51fan <abc2978723@163.com>
2018-02-05 15:06:51 +0800

    update

--- js/res.js
+++ js/res.js
@@ -1,8 +1,8 @@
--define([
--    // 加载css
--    "css!/css/style.css",
--    "css!/css/ol.css"
--    
--    // 加载第三方库js
--    
--]);
+define([
+    // 加载css
+    "css!/css/style.css",
+    "css!/css/ol.css"
+    
+    // 加载第三方库js
+    
+]);
