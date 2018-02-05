6350c047c18dd02a01ea34caa68ad9e0c9a8417e
51fan <abc2978723@163.com>
2018-02-05 14:53:29 +0800

    upadte

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ require.config({
     paths: {
         'angular': 'js/angular.min',
         'uiRouter': 'js/angular-ui-router.min',
-        'jquery': 'jquery-2.2.4.min'
+        'jquery': 'jquery-2.2.4.min',
+        
+        // 自定义
+        "res": "/res",
     },
     
     shim: {
