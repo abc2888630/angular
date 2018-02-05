785951bd6cbb77e4c27f6568bbcbedcea37930d3
51fan <abc2978723@163.com>
2018-02-05 15:19:45 +0800

    update

--- js/res.js
+++ js/res.js
@@ -1,8 +1,12 @@
 define([
     // 加载css
+    //线上github.io
     "css!/angular/css/style.css",
     "css!/angular//css/ol.css"
     
+    //本地
+    //"css!/css/style.css",
+    //"css!//css/ol.css"
     // 加载第三方库js
     
 ]);
